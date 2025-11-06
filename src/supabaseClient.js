e67ee328df84edbb562e61aeb1bb1684ca68fbd8
binkoten <161278601+Bin198900@users.noreply.github.com>
2025-11-06 07:18:08 +0000

    zz

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ocateixuzulwmrtxseom.supabase.co";
+const SUPABASE_URL = "https://nckbthxreacjxaiokjkn.supabase.co";
 const SUPABASE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYXRlaXh1enVsd21ydHhzZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMTYyMTQsImV4cCI6MjA3MTc5MjIxNH0.w7PLqjLGj4hNNGDh81NwDodEUCCqVSVm_PL0FpYWif8";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ja2J0aHhyZWFjanhhaW9ramtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMTkzMjksImV4cCI6MjA3Nzc5NTMyOX0.Ep5LFWJASC6tzidn3G5jsF9BULjf6JNq-1jFy3bReas";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
